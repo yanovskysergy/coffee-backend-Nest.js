12d69bda449635198fc9c60a29ab65589bbdef22
yanovskysergy <yanovsky.sergiy@gmail.com>
2023-07-26 02:07:24 +0200

    chage port

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
